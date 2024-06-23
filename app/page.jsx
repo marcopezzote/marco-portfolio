4f23da3401a1adf7e279008e23a83fa85721fcce
marcopezzote <pezzote@outlook.com>
2024-06-23 14:09:20 -0300

    1h12

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/*/ TEXTO */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span>Desenvolvedor Full Stack</span>
             <h1 className="h1 mb-6">
               Hello, world! Eu sou o <br />
@@ -40,7 +40,7 @@ const Home = () => {
             </div>
           </div>
           {/*/ foto */}
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
--- components/Photo.jsx
+++ components/Photo.jsx
@@ -5,18 +5,62 @@ import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div className="w-full h-full relative">
-      <motion.div>
-        <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
+    <div className="w-full h-full relative flex justify-center items-center">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+        }}
+        className="relative flex justify-center items-center"
+      >
+        {/* IMAGE */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="w-[238px] h-[238px] xl:w-[398px] xl:h-[398px] mix-blend-lighten absolute flex justify-center items-center"
+          //style={{ filter: "brightness(0.7)" }} // Adjust brightness to make the image darker
+        >
           <Image
-            src="/assets/photo2.png"
+            src="/assets/photo4.png"
             priority
             quality={100}
             fill
             alt=""
             className="object-contain"
           />
-        </div>
+        </motion.div>
+
+        {/* CIRCLE */}
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px] flex justify-center items-center"
+          fill="transparent"
+          viewbox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="220"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
